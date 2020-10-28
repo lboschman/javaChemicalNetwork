829b4d8e7eadcfaab66eeb5b6a928908948d3ec5
Leon Boschman <leon.boschman@gmail.com>
2020-10-28 18:43:05 +0100

    Added a basic chemical reaction.

---
+++ src/chemicalNetwork/Reaction.java
@@ -0,0 +1,33 @@
+package chemicalNetwork;
+
+import javax.print.DocFlavor;
+
+public class Reaction {
+    ChemicalCompound[] reactants;
+    ChemicalCompound[] products;
+    double sigma;
+    double barrier;
+
+    double boltzmannConstant =  1.3806503e-23;
+
+    public Reaction( ChemicalCompound[] reactants, ChemicalCompound[] products, double sigma, double barrier) {
+        this.reactants = reactants;
+        this.products = products;
+        this.sigma = sigma;
+        this.barrier = barrier;
+    }
+
+    public double calcRate( double tempGas) {
+        double rate = this.sigma;
+
+        for (ChemicalCompound compound: this.reactants) {
+            rate *= compound.abundance;
+        }
+
+        rate *= Math.exp(this.barrier / (boltzmannConstant * tempGas));
+
+        return rate;
+
+    }
+
+}
