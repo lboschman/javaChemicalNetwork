e55514d12e674d316044d798e58308e0de001015
Leon Boschman <leon.boschman@gmail.com>
2020-10-29 21:25:01 +0100

    Added comments for the ChemicalCompound

--- src/chemicalNetwork/ChemicalCompound.java
+++ src/chemicalNetwork/ChemicalCompound.java
@@ -1,6 +1,9 @@
 package chemicalNetwork;
 
+/** The ChemicalCompound class exists to provide chemical compounds with a name and an abundance */
+
 public class ChemicalCompound {
+    /* The compound has both a name and an abundance (concentration) */
     String name;
     double abundance;
 
@@ -10,6 +13,7 @@ public class ChemicalCompound {
     }
 
     public void setAbundance(double abundance) {
+        /* Avoid a negative abundance of the compound. */
         if (abundance > 0) {
             this.abundance = abundance;
         } else {
