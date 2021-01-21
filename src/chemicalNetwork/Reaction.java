08cf17a64258887f35714ad0934171b70a957bb6
Leon Boschman <leon.boschman@gmail.com>
2021-01-21 16:03:57 +0100

    Added comments for Reaction

--- src/chemicalNetwork/Reaction.java
+++ src/chemicalNetwork/Reaction.java
@@ -1,13 +1,43 @@
 package chemicalNetwork;
 
+/**
+ * The class Reaction links multiple ChemicalCompounds to a single chemical reaction.
+ *
+ * A gas temperature-dependent reaction rate can be calculated using the calcRate method.
+ */
+
 public class Reaction {
+    /**
+     * The ChemicalCompounds that react with each other in the reaction.
+     * Their concentration will usually decrease as the reaction runs.
+     */
     ChemicalCompound[] reactants;
+    /**
+     * The ChemicalCompounds produced in the reaction.
+     * As the reaction progresses, their concentration usually increases.
+     */
     ChemicalCompound[] products;
+    /**
+     * Cross section for the reaction.
+     * Reaction rate scales linearly with the cross section
+     */
     double sigma;
+    /**
+     * Energy barrier, creates a temperature dependence for the reaction rate, follows Arrhenius kinetics.
+     */
     double barrier;
 
-    double boltzmannConstant =  1.3806503e-23;
+    /**
+     * Boltzmann constant, necessary for Arrhenius kinetics, in kg m^2 s^-2 K^-1 (or J/K)
+     */
+    static double boltzmannConstant =  1.3806503e-23;
 
+    /**
+     * @param reactants Reacting chemical compounds
+     * @param products reaction products
+     * @param sigma reaction rate cross section
+     * @param barrier reaction barrier
+     */
     public Reaction( ChemicalCompound[] reactants, ChemicalCompound[] products, double sigma, double barrier) {
         this.reactants = reactants;
         this.products = products;
@@ -15,13 +45,19 @@ public class Reaction {
         this.barrier = barrier;
     }
 
+    /**
+     * Calculate the reaction rate for a given gas temperature.
+     *
+     * @param tempGas gas temperature
+     * @return reaction rate
+     */
     public double calcRate( double tempGas) {
         double rate = this.sigma;
 
         for (ChemicalCompound compound: this.reactants) {
             rate *= compound.abundance;
         }
-
+        /* Calculate reaction rate according to Arrhenius kinetics */
         rate *= Math.exp(- this.barrier / (boltzmannConstant * tempGas));
 
         return rate;
